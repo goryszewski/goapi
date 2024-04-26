1325b23b5b991d15656f9ff17af01064870bea22
Michal Goryszewski <goryszewski@gmail.com>
2024-04-26 22:06:42 +0200

    some code

--- main.go
+++ main.go
@@ -6,13 +6,28 @@ import (
 	"net/http"
 )
 
+var PORT string = ":8083"
+
 func page(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Page Test API")
 }
 
+func Local_Router() {
+	log.Println("Load Router")
+	http.HandleFunc("/", page)
+}
+
+func initweb(port string) {
+	log.Println("Start Main")
+	err := http.ListenAndServe(port, nil)
+
+	log.Fatal(err)
+}
+
 func main() {
 
-	http.HandleFunc("/", page)
-	log.Fatal(http.ListenAndServe(":8083", nil))
-	fmt.Println("test")
+	log.Println("Start Main")
+	Local_Router()
+	initweb(PORT)
+
 }
