e04e0502c7cfc697deb817b5785bc4ca1fe22335
Michal Goryszewski <goryszewski@gmail.com>
2024-04-30 23:15:39 +0200

    add channel and multi service

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 var PORT string = ":8083"
@@ -17,32 +18,34 @@ func Local_Router() {
 	http.HandleFunc("/", page)
 }
 
-func initweb(port string) {
-	log.Println("Start Main")
-	err := http.ListenAndServe(port, nil)
-
-	log.Fatal(err)
-}
-
 func newService(addr string) *http.Server {
 	return &http.Server{Addr: addr}
 }
 
+func buildService(port string, ch chan string) {
+	ch <- port
+	server := newService(port)
+	fmt.Printf("RUN Service Port: %s \n", port)
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Printf("Error:%p\n", err)
+	}
+	data := <-ch
+	fmt.Printf("LOG1 %s\n", data)
+}
+
 func main() {
-
-	go func(port string) {
-		server := newService(port)
-		fmt.Printf("RUN Service Port: %s \n", port)
-		err := server.ListenAndServe()
-		if err != nil {
-			fmt.Printf("Error:%p\n", err)
-		}
-		fmt.Printf("LOG1\n")
-
-	}(":8084")
-
+	ch := make(chan string, 3)
 	log.Println("Start Main")
 	Local_Router()
-	initweb(PORT)
+	services := []string{":8084", ":8083", ":8085"}
+	for _, item := range services {
 
+		go buildService(item, ch)
+	}
+
+	for {
+		time.Sleep(time.Second * 2)
+		fmt.Printf("Loop | chan size: [%x]\n", len(ch))
+	}
 }
